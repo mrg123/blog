c49077e4cf94c7279f677f5a76adea6cfc8bbc22
ccna8008 <ccna8008@163.com>
2019-09-29 12:11:16 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ require '../php/init.inc.php';
 $smarty->assign('title','Mrg123|私欲日生如地上尘,一日不扫便又有一层');
 $smarty->assign('year',date('Y'));
 
-$data = file_get_contents(DIR_CATALOG . 'html/step.md');
+$data = file_get_contents(DIR_CATALOG . 'md/step.md');
 
 $smarty->assign('data',str_replace(PHP_EOL,'\r\n',$data));
 
